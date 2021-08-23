10c580aaaace59194b14a71225411b8d51c2e03d
zenglinhui <yutanbo123@163.com>
2021-08-23 19:00:18 +0800

    add open-source

---
+++ note/java/open-source/sharding-sphere-jdbc/src/main/java/com/zenglinhui/test/JdbcTest.java
@@ -0,0 +1,30 @@
+package com.zenglinhui.test;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/**
+ * @author zenglh
+ * @date 2021/8/23 17:17
+ */
+public class JdbcTest {
+
+    public static void main(String[] args) throws SQLException {
+        DataSource dataSource = JdbcDataSourceUtil.jdbcDataSource();
+        String sql = "select order_id from t_order o where o.user_id = ? and o.order_id = ?";
+        try (Connection conn = dataSource.getConnection();
+             PreparedStatement ps = conn.prepareStatement(sql)) {
+            ps.setInt(1, 10);
+            ps.setInt(2, 1000);
+            try (ResultSet rs = ps.executeQuery()) {
+                while (rs.next()) {
+                    System.out.println(rs.getInt(1));
+                }
+            }
+        }
+    }
+
+}
---
+++ note/java/open-source/sharding-sphere-jdbc/src/test/java/com/zenglinhui/test/AppTest.java
@@ -0,0 +1,15 @@
+package com.zenglinhui.test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+    /**
+     * Rigorous Test :-)
+     *//*
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }*/
+}
